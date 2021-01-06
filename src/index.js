2da1a1199f000497a706f986508ec3e970148c3f
vladimir-skikh <19vladimir99@mail.ru>
2021-01-06 20:13:19 +0300

    Store object

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import * as serviceWorker from './serviceWorker';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addPost, changeNewPostText, addNewMessage, changeNewMessageText} from './redux/state';
 
 export let renderReactDom = (state) => {
     ReactDOM.render(
         <React.StrictMode>
-          <App state={state} addPost={addPost} addNewMessage={addNewMessage} changeNewPostText={changeNewPostText} changeNewMessageText={changeNewMessageText}/>
+          <App 
+            state={state} 
+            addPost={store.addPost.bind(store)} 
+            addNewMessage={store.addNewMessage.bind(store)} 
+            changeNewPostText={store.changeNewPostText.bind(store)} 
+            changeNewMessageText={store.changeNewMessageText.bind(store)}
+          />
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 
-renderReactDom(state);
+renderReactDom(store.getState());
 
-subscribe(renderReactDom);
+store.subscribe(renderReactDom);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,121 +1,123 @@
-let renderReactDom = () => {
-    console.log('state changed')
-}
-
-let state = {
-    newsPage: {
-        postsData: [
-            {
-                postText: "Post 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ex temporibus quia autem ab perferendis odio sunt magni, amet accusamus modi recusandae reiciendis, quae molestiae officiis iste nam aspernatur eos",
-                postPhoto: "https://themified.com/friend-finder/images/post-images/1.jpg",
-                authorAvatar: "https://themified.com/friend-finder/images/users/user-5.jpg",
-                authorName: "Alexis Clark",
-            },
-            {
-                postText: "Post 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ex temporibus quia autem ab perferendis odio sunt magni, amet accusamus modi recusandae reiciendis, quae molestiae officiis iste nam aspernatur eos",
-                postPhoto: "https://themified.com/friend-finder/images/post-images/2.jpg",
-                authorAvatar: "https://themified.com/friend-finder/images/users/user-3.jpg",
-                authorName: "Linda Lohan",
-            },
-            {
-                postText: "Post 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ex temporibus quia autem ab perferendis odio sunt magni, amet accusamus modi recusandae reiciendis, quae molestiae officiis iste nam aspernatur eos",
-                postPhoto: "https://themified.com/friend-finder/images/post-images/11.jpg",
-                authorAvatar: "https://themified.com/friend-finder/images/users/user-4.jpg",
-                authorName: "John Doe",
-            },
-        ],
-        newPostText: 'Write what you wish',
+let store = {
+    _state: {
+        newsPage: {
+            postsData: [
+                {
+                    postText: "Post 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ex temporibus quia autem ab perferendis odio sunt magni, amet accusamus modi recusandae reiciendis, quae molestiae officiis iste nam aspernatur eos",
+                    postPhoto: "https://themified.com/friend-finder/images/post-images/1.jpg",
+                    authorAvatar: "https://themified.com/friend-finder/images/users/user-5.jpg",
+                    authorName: "Alexis Clark",
+                },
+                {
+                    postText: "Post 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ex temporibus quia autem ab perferendis odio sunt magni, amet accusamus modi recusandae reiciendis, quae molestiae officiis iste nam aspernatur eos",
+                    postPhoto: "https://themified.com/friend-finder/images/post-images/2.jpg",
+                    authorAvatar: "https://themified.com/friend-finder/images/users/user-3.jpg",
+                    authorName: "Linda Lohan",
+                },
+                {
+                    postText: "Post 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ex temporibus quia autem ab perferendis odio sunt magni, amet accusamus modi recusandae reiciendis, quae molestiae officiis iste nam aspernatur eos",
+                    postPhoto: "https://themified.com/friend-finder/images/post-images/11.jpg",
+                    authorAvatar: "https://themified.com/friend-finder/images/users/user-4.jpg",
+                    authorName: "John Doe",
+                },
+            ],
+            newPostText: 'Write what you wish',
+        },
+        messagesPage: {
+            dialogsData: [
+                {
+                    id: 2,
+                    name: 'Linda Lohan',
+                    avatar: 'https://themified.com/friend-finder/images/users/user-2.jpg',
+                    lastMessage: 'Hi there, how are you',
+                },
+                {
+                    id: 10,
+                    name: 'Julia Cox',
+                    avatar: 'https://themified.com/friend-finder/images/users/user-10.jpg',
+                    lastMessage: 'see you soon',
+                },
+                {
+                    id: 3,
+                    name: 'Sophia Lee',
+                    avatar: 'https://themified.com/friend-finder/images/users/user-3.jpg',
+                    lastMessage: 'Hi there, how are you',
+                }
+            ],
+            activeDialogMessagesData: [
+                {
+                    author: "other",
+                    avatar: "https://themified.com/friend-finder/images/users/user-2.jpg",
+                    message: "Hi honey, how are you doing???? Long time no see. Where have you been?",
+                },
+                {
+                    author: "me",
+                    avatar: "https://invisionbyte.ru/test/uploads/monthly_2018_01/Wmug__uf.thumb.jpg.eca0349ccc67dd24370df4c7e452e924.jpg",
+                    message: "I'm fine, thanks! What about you?",
+                },
+            ],
+            newMessageText: 'My new message',
+        },
+        follow: {
+            followData: [
+                {
+                    name: "Diana Amber",
+                    avatar: "https://themified.com/friend-finder/images/users/user-11.jpg",
+                },
+                {
+                    name: "Cris Haris",
+                    avatar: "https://themified.com/friend-finder/images/users/user-12.jpg",
+                },
+                {
+                    name: "Brian Walton",
+                    avatar: "https://themified.com/friend-finder/images/users/user-13.jpg",
+                },
+            ],
+        },
     },
-    messagesPage: {
-        dialogsData: [
-            {
-                id: 2,
-                name: 'Linda Lohan',
-                avatar: 'https://themified.com/friend-finder/images/users/user-2.jpg',
-                lastMessage: 'Hi there, how are you',
-            },
-            {
-                id: 10,
-                name: 'Julia Cox',
-                avatar: 'https://themified.com/friend-finder/images/users/user-10.jpg',
-                lastMessage: 'see you soon',
-            },
-            {
-                id: 3,
-                name: 'Sophia Lee',
-                avatar: 'https://themified.com/friend-finder/images/users/user-3.jpg',
-                lastMessage: 'Hi there, how are you',
-            }
-        ],
-        activeDialogMessagesData: [
-            {
-                author: "other",
-                avatar: "https://themified.com/friend-finder/images/users/user-2.jpg",
-                message: "Hi honey, how are you doing???? Long time no see. Where have you been?",
-            },
-            {
-                author: "me",
-                avatar: "https://invisionbyte.ru/test/uploads/monthly_2018_01/Wmug__uf.thumb.jpg.eca0349ccc67dd24370df4c7e452e924.jpg",
-                message: "I'm fine, thanks! What about you?",
-            },
-        ],
-        newMessageText: 'My new message',
+    _renderReactDom() {},
+
+    addPost () {
+        let newPost = {
+            postText: this._state.newsPage.newPostText,
+            postPhoto: "https://themified.com/friend-finder/images/post-images/11.jpg",
+            authorAvatar: "https://themified.com/friend-finder/images/users/user-4.jpg",
+            authorName: "My name",
+        };
+        this._state.newsPage.postsData.push(newPost);
+        this._state.newsPage.newPostText = '';
+        this.renderReactDom(this._state);
     },
-    follow: {
-        followData: [
-            {
-                name: "Diana Amber",
-                avatar: "https://themified.com/friend-finder/images/users/user-11.jpg",
-            },
-            {
-                name: "Cris Haris",
-                avatar: "https://themified.com/friend-finder/images/users/user-12.jpg",
-            },
-            {
-                name: "Brian Walton",
-                avatar: "https://themified.com/friend-finder/images/users/user-13.jpg",
-            },
-        ],
+
+    addNewMessage() {
+        let newMessage = {
+            author: "me",
+            avatar: "https://invisionbyte.ru/test/uploads/monthly_2018_01/Wmug__uf.thumb.jpg.eca0349ccc67dd24370df4c7e452e924.jpg",
+            message: this._state.messagesPage.newMessageText,
+        };
+        this._state.messagesPage.activeDialogMessagesData.push(newMessage);
+        this._state.messagesPage.newMessageText = '';
+    
+        this.renderReactDom(this._state);
+    },
+
+    changeNewPostText(text) {
+        this._state.newsPage.newPostText = text;
+        this.renderReactDom(this._state);
+    },
+
+    changeNewMessageText(text) {
+        this._state.messagesPage.newMessageText = text;
+        this.renderReactDom(this._state);
+    },
+
+    subscribe(observer) {
+        this._renderReactDom = observer;
+    },
+    
+    getState() {
+        return this._state;
     },
 }
 
-
-export const addPost = () => {
-    let newPost = {
-        postText: state.newsPage.newPostText,
-        postPhoto: "https://themified.com/friend-finder/images/post-images/11.jpg",
-        authorAvatar: "https://themified.com/friend-finder/images/users/user-4.jpg",
-        authorName: "My name",
-    };
-    state.newsPage.postsData.push(newPost);
-    state.newsPage.newPostText = '';
-    renderReactDom(state);
-}
-
-export const addNewMessage = () => {
-    let newMessage = {
-        author: "me",
-        avatar: "https://invisionbyte.ru/test/uploads/monthly_2018_01/Wmug__uf.thumb.jpg.eca0349ccc67dd24370df4c7e452e924.jpg",
-        message: state.messagesPage.newMessageText,
-    };
-    state.messagesPage.activeDialogMessagesData.push(newMessage);
-    state.messagesPage.newMessageText = '';
-
-    renderReactDom(state);
-}
-
-export const changeNewPostText = (text) => {
-    state.newsPage.newPostText = text;
-    renderReactDom(state);
-}
-
-export const changeNewMessageText = (text) => {
-    state.messagesPage.newMessageText = text;
-    renderReactDom(state);
-}
-
-export const subscribe = (observer) => {
-    renderReactDom = observer;
-} 
-
-export default state;
+export default store;
