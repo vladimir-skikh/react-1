075748965bfe73956167a9c27ce395ce19b103c1
vladimir-skikh <19vladimir99@mail.ru>
2021-02-01 21:28:37 +0300

    Updates

---
+++ precache-manifest.29f7b7d29a8a5d7e5534aaecce5e92ff.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8287bddfbb7eb4a41a1d82c83994f58",
+    "url": "/react-1/index.html"
+  },
+  {
+    "revision": "b1dbfc755b07aee9b567",
+    "url": "/react-1/static/css/4.2c1247e6.chunk.css"
+  },
+  {
+    "revision": "ed7e5f0da2cdac389444",
+    "url": "/react-1/static/css/5.ec547d91.chunk.css"
+  },
+  {
+    "revision": "77f25a488f667253a3e6",
+    "url": "/react-1/static/css/6.a27099aa.chunk.css"
+  },
+  {
+    "revision": "e580ac855254975cc5ef",
+    "url": "/react-1/static/css/7.7c59be64.chunk.css"
+  },
+  {
+    "revision": "9283fbce56d957f739f5",
+    "url": "/react-1/static/css/8.42b0238d.chunk.css"
+  },
+  {
+    "revision": "3a7acf7fbaff3c69fc82",
+    "url": "/react-1/static/css/main.5125f55d.chunk.css"
+  },
+  {
+    "revision": "97d8168a4bf2f80b9100",
+    "url": "/react-1/static/js/0.59e73baf.chunk.js"
+  },
+  {
+    "revision": "47c455496d5565d02609",
+    "url": "/react-1/static/js/3.ceda899b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-1/static/js/3.ceda899b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1dbfc755b07aee9b567",
+    "url": "/react-1/static/js/4.e20edc8b.chunk.js"
+  },
+  {
+    "revision": "ed7e5f0da2cdac389444",
+    "url": "/react-1/static/js/5.d35414af.chunk.js"
+  },
+  {
+    "revision": "77f25a488f667253a3e6",
+    "url": "/react-1/static/js/6.fd7e5cf8.chunk.js"
+  },
+  {
+    "revision": "e580ac855254975cc5ef",
+    "url": "/react-1/static/js/7.301f0bd9.chunk.js"
+  },
+  {
+    "revision": "9283fbce56d957f739f5",
+    "url": "/react-1/static/js/8.910ac718.chunk.js"
+  },
+  {
+    "revision": "3a7acf7fbaff3c69fc82",
+    "url": "/react-1/static/js/main.4b61663d.chunk.js"
+  },
+  {
+    "revision": "471e721d36cf0e166e4f",
+    "url": "/react-1/static/js/runtime-main.0a343752.js"
+  },
+  {
+    "revision": "d8e9df899181e2847ffcedebb8be87c6",
+    "url": "/react-1/static/media/loader.d8e9df89.svg"
+  },
+  {
+    "revision": "d961d73a3082870d7e90b441c30152cf",
+    "url": "/react-1/static/media/undefinedUser.d961d73a.png"
+  }
+]);
